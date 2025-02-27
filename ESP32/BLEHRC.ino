0bc581859579fa5f759ad8ce67ec8b5c38ef4f01
luheiss <hl7975@mci4me.at>
2025-02-27 19:02:46 +0100

    Update BLEHRC.ino

--- ESP32/BLEHRC.ino
+++ ESP32/BLEHRC.ino
@@ -1,5 +1,4 @@
 #include <ArduinoBLE.h>
-#include <NewPing.h>
 
 // BLE UUIDs
 #define SERVICE_UUID "19B10000-E8F2-537E-4F6C-D104768A1214"
@@ -10,28 +9,18 @@ BLEService sensorService(SERVICE_UUID);
 BLECharacteristic sensorCharacteristic(CHARACTERISTIC_UUID, BLERead | BLENotify, 3); // 3 Bytes für 3 Sensoren
 
 // Ultraschallsensor-Pins
-#define LED_OUT        5
-#define TRIGGER_PIN_1  23
-#define ECHO_PIN_1     22
-#define TRIGGER_PIN_2  19
-#define ECHO_PIN_2     18
-#define TRIGGER_PIN_3  16
-#define ECHO_PIN_3     15
-#define MAX_DISTANCE 200
-
-// Ultraschallsensor-Objekte
-NewPing sonar_1(TRIGGER_PIN_1, ECHO_PIN_1, MAX_DISTANCE);
-NewPing sonar_2(TRIGGER_PIN_2, ECHO_PIN_2, MAX_DISTANCE);
-NewPing sonar_3(TRIGGER_PIN_3, ECHO_PIN_3, MAX_DISTANCE);
-
-// Zustände der Cups
-bool is_Cup1 = false;
-bool is_Cup2 = false;
-bool is_Cup3 = false;
+#define LED_OUT        13
+#define NO_1           25
+#define NO_2           26
+#define NO_3           27
 
 void setup() {
   Serial.begin(115200);
+
   pinMode(LED_OUT, OUTPUT);
+  pinMode(NO_1, INPUT);
+  pinMode(NO_2, INPUT);
+  pinMode(NO_3, INPUT);
 
   // BLE Initialisierung
   if (!BLE.begin()) {
@@ -40,7 +29,7 @@ void setup() {
   }
 
   // BLE Setup
-  BLE.setLocalName("ESP_Ultrasonic");
+  BLE.setLocalName("ESP_Modul1");
   BLE.setAdvertisedService(sensorService);
   sensorService.addCharacteristic(sensorCharacteristic);
   BLE.addService(sensorService);
@@ -51,40 +40,37 @@ void setup() {
 void loop() {
   // BLE-Verbindung prüfen
   BLEDevice central = BLE.central();
+
+  //Taktgeschwindigkeit begrenzen
+  delay(500);
+
+ //LED Blink in Search Modus
+  if(digitalRead(LED_OUT)){
+    digitalWrite(LED_OUT, LOW);
+  } else{
+    digitalWrite(LED_OUT, HIGH);
+  }
+
+  Serial.println("Searching Device!");
   if (central) {
     Serial.println("Connected to central device!");
     digitalWrite(LED_OUT, HIGH);
 
     while (central.connected()) {
-      // Ultraschallsensoren abfragen
-      unsigned int uS_1 = sonar_1.ping();
-      unsigned int uS_2 = sonar_2.ping();
-      unsigned int uS_3 = sonar_3.ping();
-
-      float distance_1 = uS_1 / US_ROUNDTRIP_CM;
-      float distance_2 = uS_2 / US_ROUNDTRIP_CM;
-      float distance_3 = uS_3 / US_ROUNDTRIP_CM;
-
-      // Cups detektieren
-      is_Cup1 = (distance_1 > 0 && distance_1 < 5);
-      is_Cup2 = (distance_2 > 0 && distance_2 < 5);
-      is_Cup3 = (distance_3 > 0 && distance_3 < 5);
 
       // Zustände in ein Byte-Array packen
-      uint8_t sensorData[3] = {is_Cup1, is_Cup2, is_Cup3};
+      uint8_t sensorData[3] = {digitalRead(NO_1), digitalRead(NO_2), digitalRead(NO_3)};
+      // Debug-Ausgabe
+      Serial.print("Cup1: "); Serial.print(sensorData[0]);
+      Serial.print(", Cup2: "); Serial.print(sensorData[1]);
+      Serial.print(", Cup3: "); Serial.println(sensorData[2]);
 
       // BLE-Charakteristik aktualisieren
       sensorCharacteristic.writeValue(sensorData, sizeof(sensorData));
 
-      // Debug-Ausgabe
-      Serial.print("Cup1: "); Serial.print(is_Cup1);
-      Serial.print(", Cup2: "); Serial.print(is_Cup2);
-      Serial.print(", Cup3: "); Serial.println(is_Cup3);
-
       delay(500); // Datenübertragungsrate
     }
-
     Serial.println("Central device disconnected!");
     digitalWrite(LED_OUT, LOW);
-  }
+  } 
 }
